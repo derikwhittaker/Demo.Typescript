274c18a5a6b24431770a23db6615fc9e1b847339
Derik Whittaker <derik@graudo.com>
2013-04-07 13:45:49 -0400

    WIP for PPT

--- Typescript.Sandbox/Typescript.Sandbox/ViewModels/Closure.js
+++ Typescript.Sandbox/Typescript.Sandbox/ViewModels/Closure.js
@@ -1,5 +1,13 @@
 var Test = (function () {
-    function Test() { }
+    function Test(first) {
+        this.firstName = "Derik";
+        this.lastName = "Whittaker";
+        this.firstName = first;
+    }
+    Test.prototype.SayName = function () {
+        var tFirstName = this.firstName;
+        alert(tFirstName + " " + this.lastName);
+    };
     return Test;
 })();
 exports.Test = Test;
--- Typescript.Sandbox/Typescript.Sandbox/ViewModels/Closure.ts
+++ Typescript.Sandbox/Typescript.Sandbox/ViewModels/Closure.ts
@@ -1,3 +1,12 @@
 export class Test {
+    firstName: string = "Derik";
+    lastName: string = "Whittaker";
 
+    constructor(first: string) {
+        this.firstName = first;
+    }
+    SayName() {
+        var tFirstName = this.firstName;
+        alert(tFirstName + " " + this.lastName);
+    }
 }
